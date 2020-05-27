519d62d6cbbb2681b1e8b80a65a23b586a695ed2
Calin-Cristian Ciubotariu <cciubotariu@elevensystems.com>
2020-05-27 11:34:49 +0300

    Reverted to monokai

--- gatsby-config.js
+++ gatsby-config.js
@@ -92,9 +92,9 @@ module.exports = {
           resolve: `gatsby-remark-vscode`,
           // All options are optional. Defaults shown here.
           options: {
-            colorTheme: 'Abyss', // Read on for list of included themes. Also accepts object and function forms.
+            colorTheme: 'Monokai', // Read on for list of included themes. Also accepts object and function forms.
             wrapperClassName: '',   // Additional class put on 'pre' tag. Also accepts function to set the class dynamically.
-            injectStyles: true,     // Injects (minimal) additional CSS for layout and scrolling
+            injectStyles: false,     // Injects (minimal) additional CSS for layout and scrolling
             extensions: [],         // Extensions to download from the marketplace to provide more languages and themes
             // extensionDataDirectory: // Absolute path to the directory where extensions will be downloaded. Defaults to inside node_modules.
               // path.resolve('extensions'),
